6d82613d54b3ae10fa7053f1ae9cc4de70e806b5
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2016-09-27 23:04:49 +0900

    added build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+rm -r build
+nwbuild -p osx64,win64 .
+mv build/WGo\ Editor/osx64 build/WGoEditor
+zip -r build/WGoEditor.osx64.zip buidl/WGoEditor
+mv build/WGoEditor build/WGo\ Editor/osx64
+mv build/WGo\ Editor/win64 build/WGoEditor
+zip -r build/WGoEditor.win64.zip buidl/WGoEditor
+mv build/WGoEditor build/WGo\ Editor/win64
