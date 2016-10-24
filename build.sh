9330e4884dc813aeb222b6b63a4fc3765495056c
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2016-10-24 18:54:12 +0900

    typo

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/bash
+
 rm -r build
 nwbuild -p osx64,win64 .
 mv build/WGo\ Editor/osx64 build/WGoEditor
-zip -r build/WGoEditor.osx64.zip buidl/WGoEditor
+zip -r build/WGoEditor.osx64.zip build/WGoEditor
 mv build/WGoEditor build/WGo\ Editor/osx64
 mv build/WGo\ Editor/win64 build/WGoEditor
-zip -r build/WGoEditor.win64.zip buidl/WGoEditor
+zip -r build/WGoEditor.win64.zip build/WGoEditor
 mv build/WGoEditor build/WGo\ Editor/win64
