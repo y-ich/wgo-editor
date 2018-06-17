0358a0052feb850c2d3933f93080b7ca96f146d6
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-06-17 15:50:04 +0900

    i18n for WGo

--- i18n.js
+++ i18n.js
@@ -1,12 +1,9 @@
 (function() {
-    if (typeof(nw) == 'undefined')
-        return
-
-    var fs = nw.require('fs');
-    var files = fs.readdirSync('i18n');
-    var langs = files.map((e) => { e.replace(/^i18n\.|\.js$/g, '') })
-    var lang = navigator.language.slice(0, 2);
-    if (!(lang in langs)) {
+    const fs = nw.require('fs');
+    const files = fs.readdirSync('wgo/i18n', { encoding: 'utf-8' });
+    const langs = files.map(e => e.replace(/^i18n\.|\.js$/g, ''));
+    let lang = navigator.language.slice(0, 2);
+    if (!langs.includes(lang)) {
         lang = 'en';
     }
     document.write(`<script src="wgo/i18n/i18n.${lang}.js"></script>`);
